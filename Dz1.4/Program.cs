6f7396401ef7fe774bb8dfc1eb73c6a7556f9ed2
AndreyOvcharenko <Andrey_ovch@mail.ru>
2022-07-18 23:43:34 +0300

    add Dz 1.4

---
+++ Dz1.4/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = new Random().Next(5, 12);
+Console.WriteLine($"День недели: {a}");
+
+ if(a < 1 | a > 7)
+{
+    Console.WriteLine($"Такого дня недели нет");  
+}
+ else
+{
+    if (a == 6 | a == 7)
+    {
+        Console.WriteLine("Да является выходным");
+    }
+    else
+    {
+        Console.WriteLine("Нет не является выходным");
+    }   
+}
+
+    
