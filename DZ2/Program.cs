38a25fb88de138152999c22069c9a241ce81ba7c
AndreyOvcharenko <Andrey_ovch@mail.ru>
2022-07-18 21:46:20 +0300

    add DZ 1.2

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -3,14 +3,8 @@ int b = new Random().Next(-3, 25);
 int c = new Random().Next(1, 5);
 int max = a;
 
-if (a > max) max = a;
-Console.WriteLine(a);
-
 if (b > max) max = b;
-Console.WriteLine(b);
-
 if (c > max) max = c;
-Console.WriteLine(c);
 
 Console.Write("max = ");
 Console.WriteLine(max);
--- Dz3/Program.cs
+++ Dz3/Program.cs
@@ -1,5 +1,7 @@
-﻿Console.Write("Введите число: ");
-int a = int.Parse(Console.ReadLine()!);
+﻿// Console.Write("Введите число: ");
+// int a = int.Parse(Console.ReadLine()!);
+int a = new Random().Next(-10, 10);
+Console.WriteLine(a);
 
 if(a % 2 == 0)
 {
