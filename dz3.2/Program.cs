4133d392e1d8d38ad4dcad9a8e5c04c3d6d62e79
AndreyOvcharenko <Andrey_ovch@mail.ru>
2022-07-21 20:46:38 +0300

    add dz 3.2

---
+++ dz3.2/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = new Random().Next(10000, 100000);
+// int a = 45654;
+int x = a / 10000;
+int y = a % 10000 / 1000;
+int b = a % 100 / 10;
+int c = a % 10;
+// Console.WriteLine($"{x} {y} {b} {c}");
+if (x == c & y == b)
+{
+    Console.WriteLine($"{a} -> да ");
+}
+else
+{
+    Console.WriteLine($"{a} -> нет");
+}
