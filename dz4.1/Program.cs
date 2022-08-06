6719a198c204de1c7b0c18cd809e5efbb0d3dcbc
AndreyOvcharenko <Andrey_ovch@mail.ru>
2022-08-06 18:29:26 +0300

    add dz 4.1

---
+++ dz4.1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите метод, который принимает на вход 
+// два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+void Print(int result)
+{
+    Console.WriteLine($"Результат: {result}");
+}
+
+int Str(int a, int b)
+{
+    Console.WriteLine($"{a}, {b}");
+    int result = Convert.ToInt32(Math.Pow(a, b));
+    return result;
+}
+
+int res = Str(6, 3);
+Print(res);
